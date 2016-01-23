4428eec1a3efb0fc554a2446d4492a7fb6b4a66c
alberto.bonfiglio@gmail.com <R0b0tics!!152>
2016-01-23 12:56:13 -0800

    Work on physics

--- classes/invertedpendulum.py
+++ classes/invertedpendulum.py
@@ -4,7 +4,7 @@
 # eq1 --> m x2dot cosΘ + ml Θ2dot = mg sinΘ
 
 
-from math import sin, cos, pi
+from math import sin, cos, pi, degrees
 from scipy import arange
 
 g = 9.8 #gravity acceleration constant (m/s**2)
@@ -31,9 +31,19 @@ class InvertedPendulum (object):
     def __getLinearAccelleration(self, u=1):
         # x2dot = ( ml sinΘ Θdot^2 - ml cosΘ Θ2dot + u) / (M+m)
         # Calculates the linear acceleration
-        x2dot = ((self.m * self.l * sin(self.theta) * self.thetadot**2) -
+        x2dot = ((self.m * self.l * sin(self.theta) * (self.thetadot**2)) -
                  (self.m * self.l * cos(self.theta) * self.theta2dot) + u) / \
                 (self.M + self.m)
+        return x2dot
+
+    def __getLinearAccelleration2(self, u=1, theta=0, thetadot=0):
+        # Calculates the linear acceleration
+        a = u + \
+            (self.m * self.l * sin(theta) * thetadot**2) - \
+            (self.m * g * cos(theta) * sin(theta))
+        b = self.M + self.m - (self.m * cos(theta)**2)
+
+        x2dot = a/b
         return x2dot
 
 
@@ -41,43 +51,72 @@ class InvertedPendulum (object):
         Θ2dot = ((g * sin(self.theta)) - (x2dot * cos(self.theta))) / self.l
         return Θ2dot
 
-    #TODO Factor in time
-    def __getLinearVelocity(self, x2dot):
-        return self.xdot + x2dot
+    def __getAngularAccelleration2(self, u=1, theta=0, thetadot=0):
+        M = self.M + self.m
+        a = (u * cos(theta)) - (M * g * sin(theta)) + (self.m * self.l * (cos(theta) * sin(theta)) * thetadot)
+        b = (self.m * self.l * (cos(theta)**2)) - (M * self.l)
 
-    def __getAngularVelocity(self, theta2dot):
-        return self.thetadot + theta2dot
+        Θ2dot = a / b
+        return Θ2dot
+
+
+    def __getLinearVelocity(self, xdot, x2dot, t):
+        try:
+            return xdot + (x2dot * t)
+        except ZeroDivisionError:
+            return xdot
+
+    def __getAngularVelocity(self, thetadot, theta2dot, t):
+        try:
+            return thetadot + (theta2dot * t)
+        except ZeroDivisionError:
+            return thetadot
 
 
 
     #applies a u force in newtons to the pendulum
-    def applyforce(self, u=1, tmax=100, timeslice=0.1):
+
+    def applyforce(self, u=1, tmax=10, timeslice=0.001):
         try:
+            xArray = []
+            thetaArray = []
+
+            x = 0
+            xdot = 0
+            theta = 0
+            thetadot = 0
+
             #Calculates the linear acceleration, and velocity
-            x2dot = self.__getLinearAccelleration(u)
+            #x2dot = self.__getLinearAccelleration(u)
+            x2dot = self.__getLinearAccelleration2(u, theta, thetadot)
 
             #Calculates the angular acceleration
-            theta2dot = self.__getAngularAccelleration(x2dot)
+            #theta2dot = self.__getAngularAccelleration(x2dot)
+            theta2dot = self.__getAngularAccelleration2(u, theta, thetadot)
 
             print('Acceleration -> {0}'.format(x2dot))
             print('Angular acceleration -> {0}'.format(theta2dot))
 
             # Once we have acceleration we can calculate velocity and positions at time t
-            x = 0
-            theta = 0
-
-            for n in arange(0, tmax, timeslice):
-                xdot = self.__getLinearVelocity(x2dot)
-                thetadot = self.__getAngularVelocity(theta2dot)
+            for t in arange(timeslice, tmax, timeslice):
+                xdot = self.__getLinearVelocity(xdot, x2dot, t)
+                thetadot = self.__getAngularVelocity(thetadot, theta2dot, t)
+                print('Vel -> {0}'.format(xdot))
+                print('Ang Vel -> {0}'.format(thetadot))
 
                 x = x + (xdot * timeslice)
                 theta = theta + (thetadot * timeslice)
 
                 print('Position -> {0}'.format(x))
-                print('Theta -> {0}'.format(theta))
+                print('Theta -> {0} - {1}'.format(theta, degrees(theta)))
 
+                xArray.append(x)
+                thetaArray.append(theta)
+
+            return xArray, thetaArray
         except Exception as ex:
             print(ex)
+
 
 
    #def applyforce(self, u=0.01, x=0, xdot=0, x2dot=0, theta=0, theta2dot=0):
--- main.py
+++ main.py
@@ -2,18 +2,102 @@
 
 
 from classes.invertedpendulum import InvertedPendulum
+from fractions import Fraction
+from math import sin, cos
+import matplotlib.pyplot as plt
+import numpy as np
+
 
 def main():
 
     #neuralNet = NeuralNetwork()
 
     pendulum = InvertedPendulum()
+    x = []
+    O = []
+    for n in np.arange(0, 10, 0.5):
+        x, O = pendulum.applyforce(u=n, tmax=0.83, timeslice=0.001)
 
-    for n in range(0, 100):
-        pendulum.applyforce(n)
+        showGraph(O)
 
-    pass
 
+
+def showGraph(theta):
+    r = 1
+    x = []
+    y = []
+    for n in range(len(theta)):
+        x.append(r*cos(theta[n]))
+        y.append(r*sin(theta[n]))
+
+    fig = plt.figure()
+    ax  = fig.add_subplot(111)
+
+    ax.plot(x,y,'b.')
+    plt.show()
+
+
+
+
+def x1():
+
+    x=np.arange(-10.0,10.0,0.1)
+    y=np.arctan(x)
+
+    fig = plt.figure()
+    ax  = fig.add_subplot(111)
+
+    ax.plot(x,y,'b.')
+
+    y_pi   = y/np.pi
+    unit   = 0.25
+    y_tick = np.arange(-0.5, 0.5+unit, unit)
+
+    y_label = [r"$-\frac{\pi}{2}$", r"$-\frac{\pi}{4}$", r"$0$", r"$+\frac{\pi}{4}$",   r"$+\frac{\pi}{2}$"]
+    ax.set_yticks(y_tick*np.pi)
+    ax.set_yticklabels(y_label, fontsize=20)
+
+    y_label2 = [r"$" + format(r, ".2g")+ r"\pi$" for r in y_tick]
+    ax2 = ax.twinx()
+    ax2.set_yticks(y_tick*np.pi)
+    ax2.set_yticklabels(y_label2, fontsize=20)
+
+    plt.show()
+
+
+def create_pi_labels(a, b, step):
+
+    max_denominator = int(1/step)
+    # i added this line and the .limit_denominator to solve an
+    # issue with floating point precision
+    # because of floataing point precision Fraction(1/3) would be
+    # Fraction(6004799503160661, 18014398509481984)
+
+    values = np.arange(a, b+step/10, step)
+    fracs = [Fraction(x).limit_denominator(max_denominator) for x in values]
+    ticks = values*np.pi
+
+    labels = []
+
+    for frac in fracs:
+        if frac.numerator==0:
+            labels.append(r"$0$")
+        elif frac.numerator<0:
+            if frac.denominator==1 and abs(frac.numerator)==1:
+                labels.append(r"$-\pi$")
+            elif frac.denominator==1:
+                labels.append(r"$-{}\pi$".format(abs(frac.numerator)))
+            else:
+                labels.append(r"$-\frac{{{}}}{{{}}} \pi$".format(abs(frac.numerator), frac.denominator))
+        else:
+            if frac.denominator==1 and frac.numerator==1:
+                labels.append(r"$\pi$")
+            elif frac.denominator==1:
+                labels.append(r"${}\pi$".format(frac.numerator))
+            else:
+                labels.append(r"$\frac{{{}}}{{{}}} \pi$".format(frac.numerator, frac.denominator))
+
+    return ticks, labels
 
 if __name__ == '__main__':
     main()
